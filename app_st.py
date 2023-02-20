8adf1c14332ddab48c704b6a00fa471f42c4f135
Danielle L. Taneyo S <saalaure@gmail.com>
2023-02-20 14:41:26 +0100

    Update app_st.py

--- app_st.py
+++ app_st.py
@@ -24,31 +24,21 @@ transform = transforms.Compose([transforms.Resize((224,224)),
 cnames = ['BabyPants', 'BabyShirt', 'womencasualshoes', 'womenchiffontop']
 # load pipeline
 new_img = 'BabyShirt_1773.jpg'
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    new_img = Image.open(uploaded_file)
+    image = plt.imread(uploaded_file)
+    st.image(image)
+
+
 mm = torch.load('entire_model.pt')
 mm.eval()
+pred = mm(transform_image(new_img))
+probs = torch.nn.functional.softmax(pred, dim=1)
+conf, classes = torch.max(probs, 1)
 
-image = plt.imread(new_img)
-st.image(image)
 
 if st.button('Calcola'):
-    if type(new_img) == 'str':
-        mm.eval()
-        pred = mm(transform_image(Image.open(new_img)))
-        probs = torch.nn.functional.softmax(pred, dim=1)
-        conf, classes = torch.max(probs, 1)
-        with open(new_img, 'rb') as f:
-          img = f.read()
-          st.write(cnames[classes.item()], 'at confidence score:{0:.2f}'.format(conf.item()))
-    else:
-        uploaded_file = st.file_uploader("Choose a file")
-        if uploaded_file is not None:
-            new_img = Image.open(uploaded_file)
-            st.write(type(new_img))
-            image = plt.imread(uploaded_file)
-            mm.eval()
-            pred = mm(transform_image(new_img))
-            probs = torch.nn.functional.softmax(pred, dim=1)
-            conf, classes = torch.max(probs, 1)
-            st.write(cnames[classes.item()], 'at confidence score : {0:.2f}'.format(conf.item()))
-            st.image(image)
+    st.image(image)
+    st.write(cnames[classes.item()], 'at confidence score : {0:.2f}'.format(conf.item()))
     
