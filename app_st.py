061e0fd36492d882fdcd218d0bdc585ffb579a6a
Danielle Taneyo <saalaure@gmail.com>
2023-02-21 06:45:49 +0100

    modified app

--- app_st.py
+++ app_st.py
@@ -14,16 +14,30 @@ def transform_image(image_bytes):
     return my_transforms(image_bytes).unsqueeze(0)
 
 
-st.title("Predict image class")
+st.title("Deep learning to predict the class of an image")
 st.markdown("This application can be used to predict "
-                " an image")
+                " the class of a clothing item")
+st.write('''The data is a subset from the Shopee-IET dataset from Kaggle. 
+           Each image in this data depicts a clothing item and 
+           the corresponding label specifies its clothing category.
+        The following possible labels: BabyPants, BabyShirt, womencasualshoes, womenchiffontop.''')
 
 transform = transforms.Compose([transforms.Resize((224,224)),
                                 transforms.ToTensor()])
 
 cnames = ['BabyPants', 'BabyShirt', 'womencasualshoes', 'womenchiffontop']
 # load pipeline
-img = 'BabyShirt_1773.jpg'
+# img = 'BabyShirt_1773.jpg'
+
+def select_img(img):
+    d = {'Image 1': 'BabyShirt_1773.jpg',
+        'Image 2': 'womencasualshoes_2240.jpg'
+        }
+    return d[img]     
+
+## Select the filename
+img_file = st.sidebar.selectbox("Choose an image", ("Image 1", "Image 2"))
+
         
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
@@ -32,6 +46,7 @@ if uploaded_file is not None:
     st.write('You uploaded this image')
     st.image(image)
 else:
+    img = select_img(img_file)
     new_img = Image.open(img)
     image = plt.imread(img)
     st.write('Your test image')
@@ -49,3 +64,4 @@ if st.button('Calcola'):
     st.image(image)
     st.write(cnames[classes.item()], 'at confidence score : {0:.2f}'.format(conf.item()))
     
+
